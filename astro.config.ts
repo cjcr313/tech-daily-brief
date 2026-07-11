7bda7d47870953c8c1876fdf26b225f6723efa89
Carlos Jacob <walle@atlas.local>
2026-07-11 14:59:53 -0400

    Fix locale for i18n in Astro config

--- astro.config.ts
+++ astro.config.ts
@@ -30,8 +30,8 @@ export default defineConfig({
     }),
   ],
   i18n: {
-    locales: ["en"],
-    defaultLocale: "en",
+    locales: ["es", "en"],
+    defaultLocale: "es",
     routing: {
       prefixDefaultLocale: false,
     },
